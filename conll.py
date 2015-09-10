2c6e51bbe0f35f8b5a92a0ed1e1f1ee67b9f260b
Cristian Cardellino <crscardellino@gmail.com>
2015-09-10 11:56:16 -0300

    Added preprocessing for annotated documents of the NLL2RDF corpus

---
+++ conll.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from utils import NLL2RDF_CLASSES
+
+
+class WordInstance(object):
+    def __init__(self, word, label=None, tag=None, dependency=None, head=None, position=None):
+        self.word = word
+        self.label = label
+        self.tag = tag
+        self.dependency = dependency
+        self.head = head
+        self.position = position
+
+    def get_word_lemma(self):
+        return self.word.lower()  # TODO: Use a lemmatizer?
+
+    def __str__(self):
+        if self.label:
+            return "%s\t%s" % (self.word, NLL2RDF_CLASSES[self.label])
+        else:
+            return "%d\t%s\t%s\t%s\t%s" % (self.position, self.word, self.tag, self.dependency, sel.head)
+
+    def __repr__(self):
+        return str(self)
+
+
+class CoNLLSentence(object):
+    def __init__(self, words):
+        self.words = words
+
+    def __getitem__(self, item):
+        return self.words[item]
+
+    def get_head_of_word(self, position):
+        if self[position].head == 0:
+            return "root"
+        else:
+            return self[self[position].head - 1]
+
+    def get_window(self, position, window_size=5):
+        ws = int(window_size / 2)
+
+        return self.words[position-ws:position+ws+1]
+
+    def __str__(self):
+        return "\n".join([str(word) for word in self.words])
+
+    def __repr__(self):
+        return str(self)
+
+
+class CoNLLDocument(object):
+    def __init__(self, sentences):
+        self.sentences = sentences
+
+    def __getitem__(self, item):
+        return self.sentences[item]
+
+    def __str__(self):
+        return "\n\n".join([str(sentence) for sentence in self.sentences])
+
+    def __repr__(self):
+        return str(self)
---
+++ preprocess.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import re
+from conll import *
+from utils import *
+
+
+def parse_annotated_document(document_path):
+    sentences = []
+    words = []
+    odrl_action = 0
+
+    with open(document_path, "r") as input_file:
+        for line in input_file.readlines():
+            if re.match(r'^\s*$', line.strip()):
+                sentences.append(CoNLLSentence(words))
+                words = []
+
+            elif re.match(r'^#id-[0-9]+$', line.strip()):
+                continue
+
+            else:
+                token_data = line.split()
+
+                if ODRL_CLASSES_MAPPINGS[token_data[10]] == 1:
+                    nll2rdf_class = (1, 0)
+                else:
+                    odrl_action = ODRL_ACTIONS_MAPPINGS[token_data[11]] if len(token_data) == 12 else odrl_action
+
+                    nll2rdf_class = (
+                        ODRL_CLASSES_MAPPINGS[token_data[10]],
+                        odrl_action
+                    )
+
+                words.append(
+                    WordInstance(
+                        word=token_data[1],
+                        label=NLL2RDF_CLASSES_MAPPINGS[nll2rdf_class],
+                        tag=token_data[3],
+                        dependency=token_data[7],
+                        head=token_data[6],
+                        position=token_data[0]
+                    )
+                )
+
+    return CoNLLDocument(sentences)
